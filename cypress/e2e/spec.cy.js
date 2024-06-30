4402e0a9527d425463813a8a69ddde9192837300
IndraMatsiendra <imcdindra@hotmail.com>
2024-06-30 12:48:46 -0300

    adicionando 3 novos testes E2E ao cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,67 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li label')
+      .dblclick();
+
+    cy.get('.todo-list li .edit')
+      .clear()
+      .type('TP2 de Engenharia de Software - teste de edição{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.text', 'TP2 de Engenharia de Software - teste de edição');
+  });
+
+  it('Marca todas as tarefas como feitas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.toggle-all-label')
+      .click();
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Deleta todas as tarefa completadas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES - teste edição{enter}')
+      .type('TP2 de ES - teste marcar tudo como feito{enter}')
+      .type('TP2 de ES - teste deletar todas as tarefas já feitas{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .eq(0)
+      .click();
+    
+    cy.get('.todo-list li .toggle')
+      .eq(1)
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES - teste deletar todas as tarefas já feitas');
+  });
+
 });
